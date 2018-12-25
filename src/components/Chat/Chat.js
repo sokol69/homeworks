0797da3135443079b03ed8cc0b3b17be02c3da2d
sokol69 <sokolov333d@gmail.com>
2018-12-25 07:02:12 +0300

    changed method sendMessageOnEnter

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -6,7 +6,7 @@ class Chat extends Component {
 	state = {
 		messages: [],
 		messageInput: ''
-	}
+	};
 
 	render() {
 		return(
@@ -24,24 +24,28 @@ class Chat extends Component {
 					onKeyUp={this.sendMessageOnEnter}
 				/>
 			</div>
-		)
-	}
+		);
+	};
 
 	changeInputMessage = (event) => {
 		this.setState({
 			messageInput: event.target.value
-		})
-	}
+		});
+	};
 
 	sendMessageOnEnter = (event) => {
-		if (event.keyCode === 13 && this.state.messageInput) {
+		if(event.key !== 'Enter') {
+			return
+		} else if (this.state.messageInput === '') {
+			return
+		} else {
 			this.setState({
 				messages: [{text: this.state.messageInput, id: Date.now().toString()}, ...this.state.messages],
 				messageInput: ''
-			})
+			});
 			event.target.value = '';
-		}
-	}
-}
+		};
+	};
+};
 
 export default Chat;
